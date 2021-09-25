c91a17ca4a8d997ba9d4ee5f687fd60cb65330ee
Ourai Lin <ourairyu@gmail.com>
2021-09-25 10:46:30 +0800

    refactor: `TextArea` component interface

--- packages/petals/src/text-area/typing/interfaces.ts
+++ packages/petals/src/text-area/typing/interfaces.ts
@@ -2,8 +2,8 @@ import { ResizeType } from '../../basic';
 import { ITextualFormControl } from '../../form-control';
 
 interface ITextAreaComponent extends ITextualFormControl {
-  readonly row: number;
-  readonly showWordLimit: boolean;
+  readonly rows: number;
+  readonly wordage: boolean;
   readonly resize: ResizeType;
 }
 
