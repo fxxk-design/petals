59feb7b396c1fe36193356acc849cdc31234d177
Ourai Lin <ourairyu@gmail.com>
2021-10-09 22:28:11 +0800

    feat: visibility-related props and events of `Popup` component

--- packages/petals/src/popup/typing/interfaces.ts
+++ packages/petals/src/popup/typing/interfaces.ts
@@ -4,8 +4,10 @@ interface IPopupComponent {
   readonly content: string;
   readonly placement: PopupPlacement;
   readonly trigger: PopupTrigger;
+  readonly visible: boolean;
   readonly disabled: boolean;
   readonly popupClassName: string;
+  onVisibleChange(visible: boolean): void;
 }
 
 export { IPopupComponent };
