df0580e0b9e2bbda1f138b50489c84ee199ce308
Ourai Lin <ourairyu@gmail.com>
2021-10-27 17:42:20 +0800

    refactor: self-implemented `classnames`

--- packages/petals/src/basic/helper/utils.ts
+++ packages/petals/src/basic/helper/utils.ts
@@ -1,7 +1,6 @@
-import { isArray } from '@ntks/toolbox';
-import classnames from 'classnames';
+import { isString, isNumber, isArray, isLooseObject, hasOwnProp } from '@ntks/toolbox';
 
-import { LooseSize, VariantClassName, ClassName } from '../typing';
+import { LooseSize } from '../typing';
 
 function getDescendantClassName(componentName: string, descendant: string): string {
   return `${componentName}-${descendant}`;
@@ -23,10 +22,41 @@ function normalizeLooseSize(size: LooseSize): string {
   return size;
 }
 
-function normalizeClassName(className: ClassName): string {
-  return isArray(className)
-    ? classnames(...(className as VariantClassName[]))
-    : classnames(className);
+// Modified from https://www.npmjs.com/package/classnames
+function normalizeClassName(...args: any[]): string {
+  const classNames: string[] = [];
+
+  for (let i = 0; i < args.length; i++) {
+    const className = args[i];
+
+    if (!className) {
+      continue;
+    }
+
+    if (isString(className) || isNumber(className)) {
+      classNames.push(className);
+    } else if (isArray(className)) {
+      if (className.length > 0) {
+        const nestedClassName = normalizeClassName(...className);
+
+        if (nestedClassName) {
+          classNames.push(nestedClassName);
+        }
+      }
+    } else if (isLooseObject(className)) {
+      if (className.toString === Object.prototype.toString) {
+        for (const key in className) {
+          if (hasOwnProp(key, className) && className[key]) {
+            classNames.push(key);
+          }
+        }
+      } else {
+        classNames.push(className.toString());
+      }
+    }
+  }
+
+  return classNames.join(' ');
 }
 
 export { getDescendantClassName, getModifierClassName, normalizeLooseSize, normalizeClassName };
