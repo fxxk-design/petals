be42e479a0544d83b5e04c54dc548d4c1e35925b
Ourai Lin <ourairyu@gmail.com>
2022-01-23 14:37:03 +0800

    feat: `onClick` of `Link` component

--- packages/petals/src/link/typing/interfaces.ts
+++ packages/petals/src/link/typing/interfaces.ts
@@ -3,6 +3,7 @@ import { LinkTarget } from './aliases';
 interface ILinkComponent<LinkHref = string> {
   readonly href: LinkHref;
   readonly target: LinkTarget;
+  readonly onClick: () => void;
 }
 
 export { ILinkComponent };
